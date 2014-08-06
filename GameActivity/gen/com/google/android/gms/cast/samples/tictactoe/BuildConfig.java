d2d0dd5cc161b8eec7625757a9a0b8ffa1cebfd3
Natalie Kacik <njk49@cornell.edu>
2014-08-05 21:16:22 -0400

    Started configuring client side
    Started setting up Google Play Services.

---
+++ GameActivity/gen/com/google/android/gms/cast/samples/tictactoe/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.android.gms.cast.samples.tictactoe;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ ModeSelectActivity/gen/com/google/android/gms/cast/samples/democastplayer/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.google.android.gms.cast.samples.democastplayer;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
